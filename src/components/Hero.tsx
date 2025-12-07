e662d580e7558980fabd37761409144c59c46bd0
Lucas <195580638+luchersou@users.noreply.github.com>
2025-12-07 18:25:09 -0300

    refactor: Improve image loading performance on Login and Signup pages and add TextGenerateEffect to Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,28 +2,30 @@ import bannerImg from "../assets/banner.jpg";
 import { Link } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Search, Globe, Heart } from "lucide-react";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 
 export const Hero = () => {
   return (
-    <div
-      className="
-        w-full 
-        relative 
-        overflow-hidden h-[100vh] 
-      "
-    >
+    <div className="w-full relative overflow-hidden h-[100vh]">
+      
       <img
         src={bannerImg}
         alt="Banner"
         className="w-full h-full object-cover"
       />
+
       <div className="absolute inset-0 backdrop-blur-md bg-black/20 flex flex-col items-center justify-center px-4 text-center">
-        <h1 className="text-white text-3xl sm:text-4xl md:text-5xl font-bold drop-shadow-lg leading-tight">
-          Search your favorite recipe
-        </h1>
-        <p className="text-white/90 text-lg pt-5 md:text-xl font-light mb-6 max-w-2xl mx-auto text-center leading-relaxed">
-          From quick meals to global flavors — inspiration for every moment, just one click away.
-        </p>
+
+        <TextGenerateEffect
+          className="text-white text-3xl sm:text-4xl md:text-5xl font-bold drop-shadow-lg leading-tight"
+          words="Search your favorite recipe"
+        />
+
+        <TextGenerateEffect
+          className="text-white/90 text-lg pt-5 md:text-xl font-light mb-6 max-w-2xl mx-auto text-center leading-relaxed"
+          words="From quick meals to global flavors — inspiration for every moment, just one click away."
+        />
+
         <div className="flex items-center justify-center gap-6 text-white/80 text-sm mb-8">
           <div className="flex items-center gap-2">
             <Globe className="w-5 h-5" />
@@ -34,7 +36,7 @@ export const Hero = () => {
             <span>Save Favorites</span>
           </div>
         </div>
-    
+
         <div className="mt-6">
           <Link to="/filter">
             <Button 
@@ -44,6 +46,7 @@ export const Hero = () => {
             </Button>
           </Link>
         </div>
+
       </div>
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -10,7 +10,7 @@ export const Login = () => {
         <img 
           src={bgLogin}
           alt="Background"
-          loading="eager"
+          fetchPriority="high"
           className="
             absolute inset-0 
             w-full h-full
